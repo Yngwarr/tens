7b1ddf1eeba5cbf2963dc2138783ac58a1b9f47e
Yngvarr <k.yngvarr@gmail.com>
2024-08-08 00:23:42 +0200

    add area to highlight

--- src/Highlight.gd
+++ src/Highlight.gd
@@ -1,6 +1,12 @@
 class_name Highlight
 extends Panel
 
+@export var collider: CollisionShape2D
+
 func resize(rect: Rect2) -> void:
     size = rect.size
     position = rect.position
+
+    collider.shape.size = rect.size
+    collider.position = rect.size / 2.
+    # collider.position = rect.position
