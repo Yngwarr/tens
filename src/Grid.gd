bf758cbdf73e2ea84c0c9a0cabd16930496e550a
Yngvarr <k.yngvarr@gmail.com>
2025-10-06 22:57:12 +0200

    style: apply formatting to Grid

--- src/Grid.gd
+++ src/Grid.gd
@@ -19,145 +19,154 @@ var second_point := Vector2.ZERO
 var appear_step: int = 0
 var is_held := false
 
+
 func _ready() -> void:
-    var step := Game.CELL_SIZE
+	var step := Game.CELL_SIZE
 
-    for y in range(height):
-        for x in range(width):
-            var node := inner_node.instantiate() as NumberCell
-            node.position.x = step * (x + .5) - step * width / 2.
-            node.position.y = step * (y + .5) - step * height / 2.
-            node.front_layer = front_layer
-            add_child(node)
-            node.set_value(randi_range(1, Game.TARGET_SUM - 1))
+	for y in range(height):
+		for x in range(width):
+			var node := inner_node.instantiate() as NumberCell
+			node.position.x = step * (x + .5) - step * width / 2.
+			node.position.y = step * (y + .5) - step * height / 2.
+			node.front_layer = front_layer
+			add_child(node)
+			node.set_value(randi_range(1, Game.TARGET_SUM - 1))
 
-            if not Engine.is_editor_hint():
-                node.pressed.connect(on_cell_pressed)
-                node.moved_to.connect(on_cell_moved)
+			if not Engine.is_editor_hint():
+				node.pressed.connect(on_cell_pressed)
+				node.moved_to.connect(on_cell_moved)
+
 
 func _input(event: InputEvent) -> void:
-    if Engine.is_editor_hint():
-        return
+	if Engine.is_editor_hint():
+		return
 
-    if not event is InputEventMouseButton:
-        return
-    if event.pressed:
-        return
+	if not event is InputEventMouseButton:
+		return
+	if event.pressed:
+		return
 
-    on_released()
+	on_released()
+
 
 func on_cell_pressed(cell: NumberCell) -> void:
-    var pos := cell.global_position
-    first_point = pos
-    second_point = pos
-    is_held = true
-    update_highlight()
-    grabbed.emit()
+	var pos := cell.global_position
+	first_point = pos
+	second_point = pos
+	is_held = true
+	update_highlight()
+	grabbed.emit()
+
 
 func on_released() -> void:
-    if not is_held:
-        return
+	if not is_held:
+		return
 
-    is_held = false
-    released.emit()
+	is_held = false
+	released.emit()
+
 
 func on_cell_moved(cell: NumberCell) -> void:
-    if not is_held:
-        return
+	if not is_held:
+		return
 
-    var pos := cell.global_position
-    second_point = pos
-    update_highlight()
+	var pos := cell.global_position
+	second_point = pos
+	update_highlight()
+
 
 func update_highlight() -> void:
-    var left_point: Vector2
-    var right_point: Vector2
+	var left_point: Vector2
+	var right_point: Vector2
 
-    if first_point.x <= second_point.x && first_point.y <= second_point.y:
-        # print("2")
-        left_point = first_point
-        right_point = second_point
-    elif first_point.x > second_point.x && first_point.y <= second_point.y:
-        # print("3")
-        left_point = Vector2(second_point.x, first_point.y)
-        right_point = Vector2(first_point.x, second_point.y)
-    elif first_point.x <= second_point.x && first_point.y > second_point.y:
-        # print("1")
-        left_point = Vector2(first_point.x, second_point.y)
-        right_point = Vector2(second_point.x, first_point.y)
-    else:
-        # print("4")
-        left_point = second_point
-        right_point = first_point
+	if first_point.x <= second_point.x && first_point.y <= second_point.y:
+		# print("2")
+		left_point = first_point
+		right_point = second_point
+	elif first_point.x > second_point.x && first_point.y <= second_point.y:
+		# print("3")
+		left_point = Vector2(second_point.x, first_point.y)
+		right_point = Vector2(first_point.x, second_point.y)
+	elif first_point.x <= second_point.x && first_point.y > second_point.y:
+		# print("1")
+		left_point = Vector2(first_point.x, second_point.y)
+		right_point = Vector2(second_point.x, first_point.y)
+	else:
+		# print("4")
+		left_point = second_point
+		right_point = first_point
 
-    var x := left_point.x - Game.CELL_SIZE / 2.
-    var y := left_point.y - Game.CELL_SIZE / 2.
-    var w := right_point.x + Game.CELL_SIZE / 2. - x
-    var h := right_point.y + Game.CELL_SIZE / 2. - y
+	var x := left_point.x - Game.CELL_SIZE / 2.
+	var y := left_point.y - Game.CELL_SIZE / 2.
+	var w := right_point.x + Game.CELL_SIZE / 2. - x
+	var h := right_point.y + Game.CELL_SIZE / 2. - y
 
-    var rect := Rect2(x, y, abs(w), abs(h))
+	var rect := Rect2(x, y, abs(w), abs(h))
 
-    highlight_changed.emit(rect)
+	highlight_changed.emit(rect)
+
 
 func appear() -> void:
-    appear_timer.timeout.connect([
-        appear_corner,
-        appear_down,
-        appear_left
-    ].pick_random())
-    appear_timer.start()
+	appear_timer.timeout.connect([appear_corner, appear_down, appear_left].pick_random())
+	appear_timer.start()
+
 
 func appear_corner() -> void:
-    if appear_step == width + height:
-        finish_appearing()
-        return
+	if appear_step == width + height:
+		finish_appearing()
+		return
 
-    for y in range(0, height):
-        var x = appear_step - y
-        if x < 0 || x >= width:
-            continue
+	for y in range(0, height):
+		var x = appear_step - y
+		if x < 0 || x >= width:
+			continue
 
-        get_child(x + y * width).appear()
+		get_child(x + y * width).appear()
 
-    appear_step += 1
-    if appear_sound:
-        appear_sound.play()
+	appear_step += 1
+	if appear_sound:
+		appear_sound.play()
+
 
 func appear_down() -> void:
-    if appear_step == height:
-        finish_appearing()
-        return
+	if appear_step == height:
+		finish_appearing()
+		return
 
-    var start := appear_step * width
-    var end := start + width
+	var start := appear_step * width
+	var end := start + width
 
-    for i in range(start, end):
-        get_child(i).appear()
+	for i in range(start, end):
+		get_child(i).appear()
 
-    appear_step += 1
-    if appear_sound:
-        appear_sound.play()
+	appear_step += 1
+	if appear_sound:
+		appear_sound.play()
+
 
 func appear_left() -> void:
-    if appear_step == width:
-        finish_appearing()
-        return
+	if appear_step == width:
+		finish_appearing()
+		return
 
-    for i in range(appear_step, height * width, width):
-        get_child(i).appear()
+	for i in range(appear_step, height * width, width):
+		get_child(i).appear()
 
-    appear_step += 1
-    if appear_sound:
-        appear_sound.play()
+	appear_step += 1
+	if appear_sound:
+		appear_sound.play()
+
 
 func finish_appearing() -> void:
-    appear_timer.stop()
-    fully_appeared.emit()
+	appear_timer.stop()
+	fully_appeared.emit()
+
 
 func duck() -> void:
-    for c in get_children():
-        c.bounce()
+	for c in get_children():
+		c.bounce()
+
 
 func fall_apart() -> void:
-    for c in get_children():
-        c.remove()
+	for c in get_children():
+		c.remove()
