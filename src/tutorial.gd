3dac4e7a1eb55a5377bdc6061cf914d5a5cfddcd
Yngvarr <k.yngvarr@gmail.com>
2025-10-07 12:36:56 +0200

    feat: add text and the close button to the tutorial screen

--- src/tutorial.gd
+++ src/tutorial.gd
@@ -3,11 +3,18 @@ extends CanvasLayer
 
 @export_group("Internal")
 @export var grid: Grid
+@export var quit_button: Button
 
 
 func _ready() -> void:
 	if not visible:
 		queue_free()
 	else:
+		quit_button.pressed.connect(close)
 		get_tree().paused = true
 		grid.appear()
+
+
+func close() -> void:
+	queue_free()
+	get_tree().paused = false
