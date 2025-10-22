91becb7aab9635d14ea1d6ddd95295ee2ba5714e
Yngvarr <k.yngvarr@gmail.com>
2025-10-22 22:02:29 +0200

    style: apply formatting to sum_label

--- src/sum_label.gd
+++ src/sum_label.gd
@@ -5,24 +5,25 @@ extends Control
 @export var view: Label
 @export var anim: AnimationPlayer
 
+
 func _ready() -> void:
 	view.visible = false
 
+
 func update_text(value: int) -> void:
 	var is_target := value == Game.TARGET_SUM
-	
-	if value == 0: 
+
+	if value == 0:
 		view.visible = false
-	else: 
+	else:
 		view.visible = true
-	
+
 	view.text = ("%d!" % value) if is_target else str(value)
-	
+
 	if is_target:
-		view.label_settings.outline_color = Color.FOREST_GREEN 
+		view.label_settings.outline_color = Color.FOREST_GREEN
 	else:
 		view.label_settings.outline_color = Color.DARK_RED
-
 
 	if is_target:
 		anim.play(&"bounce")
