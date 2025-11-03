b7a65a4ea5082f1b32249ca5bd606e03f8a8d379
syudzius <syudzius@gmail.com>
2025-11-03 10:56:10 +0100

    return old tutorial

--- src/UI/main_menu.gd
+++ src/UI/main_menu.gd
@@ -9,6 +9,7 @@ extends Node2D
 @export var stats_window: PopupPanel
 @export var options_window: PopupPanel
 @export var wallpaper_window: PopupPanel
+@export var tutorial_button: BaseButton
 
 @export_group("Prefabs")
 @export var tutorial_prefab: PackedScene
@@ -21,15 +22,14 @@ func _ready() -> void:
 
 	credits_window.visibility_changed.connect(on_credits_visibility)
 	stats_window.visibility_changed.connect(on_stats_visibility)
-	#tutorial_button.pressed.connect(on_show_tutorial)
 	options_window.visibility_changed.connect(on_options_visibility)
 	wallpaper_window.visibility_changed.connect(on_wallpaper_visibility)
+	tutorial_button.pressed.connect(on_show_tutorial)
 	ScreenFader.hide()
 
 
 func on_credits_visibility() -> void:
 	ScreenFader.visible = credits_window.visible
-
 
 func on_stats_visibility() -> void:
 	ScreenFader.visible = stats_window.visible
@@ -39,3 +39,7 @@ func on_options_visibility() -> void:
 	
 func on_wallpaper_visibility() -> void:
 	ScreenFader.visible = wallpaper_window.visible
+	
+func on_show_tutorial() -> void:
+	var tutorial := tutorial_prefab.instantiate()
+	add_child(tutorial)
