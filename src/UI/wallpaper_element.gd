2e5d1ca392afdbfefac26fdc677b0f2f783dffb9
syudzius <syudzius@gmail.com>
2025-11-05 15:51:52 +0100

    tappable wallpapers, prototype

--- src/UI/wallpaper_element.gd
+++ src/UI/wallpaper_element.gd
@@ -1,9 +1,32 @@
 class_name WallpaperElement
-extends Button
+extends Control
 
 @export_group("Internal")
 @export var art: TextureRect
+@export var selected: NinePatchRect
+@export var locked: NinePatchRect
+@export var button: Button
+@export var anim: AnimationPlayer
+
+var star_count = 6
+var stars_needed = 5
 
 
 func init(texture: Texture2D) -> void:
 	art.texture = texture
+
+func _ready() -> void:
+	button.pressed.connect(on_wallpaper_pressed)
+	if star_count <= stars_needed:
+		locked.visible = true
+	else:
+		locked.visible = false
+
+func on_wallpaper_pressed() -> void:
+	if star_count >= stars_needed:
+		if selected.visible == true:
+			selected.visible = false
+		else:
+			selected.visible = true
+	else:
+		anim.play("bounce")
