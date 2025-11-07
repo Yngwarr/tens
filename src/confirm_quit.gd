47fec546c9154d9ccd397171849ef06ac6918d8a
Yngvarr <k.yngvarr@gmail.com>
2025-11-07 01:01:49 +0100

    send gameplayStop on quit to menu

--- src/confirm_quit.gd
+++ src/confirm_quit.gd
@@ -1,9 +1,21 @@
 extends PopupPanel
 
+@export_file("*.tscn") var next_scene: String
+
+@export_group("Internal")
 @export var button_no: Button
+@export var button_yes: Button
+
 
 func _ready() -> void:
-	button_no.pressed.connect(on_pressed)
-	
-func on_pressed() -> void:
+	button_no.pressed.connect(on_no_pressed)
+	button_yes.pressed.connect(on_yes_pressed)
+
+
+func on_no_pressed() -> void:
 	hide()
+
+
+func on_yes_pressed() -> void:
+	PokiSDK.gameplayStop()
+	get_tree().change_scene_to_file(next_scene)
